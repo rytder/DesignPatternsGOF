8168ec24431f333344b2e9f8c0045982a8710a98
Rytis <rdereskevicius@gmail.com>
2017-12-03 14:43:45 +0200

    Proxy design patter implemented

---
+++ Proxy/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy("SomeLegitPassword");
+            proxy.DoHeavyLifting();
+        }
+    }
+
+    interface ISubject
+    {
+        void DoHeavyLifting();
+    }
+
+    class Proxy : ISubject
+    {
+
+        RealSubject realThing;
+        string password;
+
+        public Proxy(string password)
+        {
+            this.password = password;
+        }
+
+        public void DoHeavyLifting()
+        {
+            if (Auth(password))
+            {
+                realThing = new RealSubject();
+                realThing.DoHeavyLifting();
+            }
+            else
+            {
+                Console.WriteLine("Did not authenticate - controlling when a costly object needs to be instantiated and initialized");
+            }
+        }
+
+        public bool Auth(string password)
+        {
+            if(password == "SomeLegitPassword")
+            {
+                return true;       
+            }
+            return false;
+        }
+
+    }
+
+    class RealSubject : ISubject
+    {
+        public void DoHeavyLifting()
+        {
+            Console.WriteLine("I am doing some real heavy lifting");
+        }
+    }
+
+
+}
